1c8969b0315a05e8cc8bf6ab596d59f411ba5347
Alex <alexbarke002@gmail.com>
2018-04-19 17:21:24 +0800

    added query for Closest Hawkers in Address Controller

--- Controllers/AddressController.cs
+++ Controllers/AddressController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,19 +8,65 @@ using HawkerFinder.Data;
 using HawkerFinder.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace HawkerFinder.Controllers {
-    public class AddressController : Controller {
-        private readonly HawkerContext _context;
-        public AddressController (HawkerContext context) {
-            _context = context;
-        }
-        public async Task<IActionResult> Index () {
-            return View (await _context.Addresses.ToListAsync ());
-        }
+  public class AddressController : Controller {
+    private readonly HawkerContext _context;
 
-        public IActionResult Error () {
-            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    private const string closestHawkersQuery =
+      @"declare @mylat fLOAT, @mylong FLOAT
+        Select @mylat = 1.297796, @mylong = 103.766223
+        SELECT top 5 ABS(dbo.DictanceKM(@mylat, Address.latitude, @mylong, Address.longitude)) DistanceKm, * from Address
+        ORDER BY ABS(dbo.DictanceKM(@mylat, Address.latitude, @mylong, Address.longitude)) ";
+    public AddressController (HawkerContext context) {
+      _context = context;
     }
+    public async Task<IActionResult> Index () {
+      return View (await _context.Addresses.ToListAsync ());
+    }
+
+    // returns some of the closest hawker centres to a given coordinate
+    public IActionResult CloseHawkers () {
+      // run query
+      Address[] closestAddresses = GetClosestAddrs ();
+      // return results
+      return View (closestAddresses);
+    }
+
+    private Address[] GetClosestAddrs () {
+      // only return up to five addresses
+      Address[] closestAddresses = new Address[5];
+      int index = 0;
+      using (SqlConnection conn =
+        new SqlConnection (_context.Database.GetDbConnection ().ConnectionString)) {
+        conn.Open ();
+        using (SqlCommand cmd = new SqlCommand (closestHawkersQuery, conn)) {
+          using (SqlDataReader dr = cmd.ExecuteReader ()) {
+            while (dr.Read () && index < 5) {
+              double dist = dr.GetDouble (0);
+              int id = dr.GetInt32 (1);
+              string address = dr.GetString (2);
+              string locationName = dr.GetString (3);
+              double latitude = dr.GetDouble (4);
+              double longitude = dr.GetDouble (5);
+              Address addr = new Address {
+                Id = id,
+                Name = locationName,
+                Addr = address,
+                latitude = latitude,
+                longitude = longitude
+              };
+              closestAddresses[index++] = addr;
+            }
+          }
+        }
+      }
+      return closestAddresses;
+    }
+
+    public IActionResult Error () {
+      return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
--- SQLQueries/queries.sql
+++ SQLQueries/queries.sql
@@ -1,44 +1,3 @@
---Select * 
---From HawkerCentres.dbo.Address
---Order By latitude, longitude
-
---Drop table HawkerCentres.dbo.Address
-
-
-/*Use HawkerCentres
-SELECT dbo.Address.latitude as lat, Address.longitude as lon, SQRT(
-    POWER(69.1 * (lat - [startlat]), 2) +
-    POWER(69.1 * ([startlng] - longitude) * COS(latitude / 57.3), 2)) AS distance
-HawkerCentres.dbo.Address HAVING distance < 25 ORDER BY distance;*/
-
-/*Use HawkerCentres
-SELECT top 100
-(
-   3959 *
-   acos(cos(radians(37)) * 
-   cos(radians(Address.latitude)) * 
-   cos(radians(Address.longitude) - 
-   radians(-122)) + 
-   sin(radians(37)) * 
-   sin(radians(Address.latitude )))
-) AS distance 
-FROM Address
---GROUP BY Address.Id, Address.Name
---HAVING distance < 25 
-ORDER BY distance; --LIMIT 0, 20;*/
-
-
-/*CREATE FUNCTION dbo.DictanceKM(@lat1 FLOAT, @lat2 FLOAT, @lon1 FLOAT, @lon2 FLOAT)
-RETURNS FLOAT 
-AS
-BEGIN
-
-    RETURN ACOS(SIN(PI()*@lat1/180.0)*SIN(PI()*@lat2/180.0)+COS(PI()*@lat1/180.0)*COS(PI()*@lat2/180.0)*COS(PI()*@lon2/180.0-PI()*@lon1/180.0))*6371
-END
-*/
-
---Use HawkerCentres
---Select * from DictanceKM(1.00, 100.00, 4.00, 105.00)
 
 
 /*declare @lat float, @lng float
@@ -55,7 +14,16 @@ SELECT ABS(dbo.DictanceKM(@lat, Latitude, @lng, Longtitude)) DistanceKm, * FROM 
 ORDER BY ABS(dbo.DictanceKM(@lat, Latitude, @lng, Longtitude))
 */
 
+/*CREATE FUNCTION dbo.DictanceKM(@lat1 FLOAT, @lat2 FLOAT, @lon1 FLOAT, @lon2 FLOAT)
+RETURNS FLOAT 
+AS
+BEGIN
+
+    RETURN ACOS(SIN(PI()*@lat1/180.0)*SIN(PI()*@lat2/180.0)+COS(PI()*@lat1/180.0)*COS(PI()*@lat2/180.0)*COS(PI()*@lon2/180.0-PI()*@lon1/180.0))*6371
+END*/
+
+
 declare @mylat fLOAT, @mylong FLOAT
 Select @mylat = 1.297796, @mylong = 103.766223
-SELECT ABS(dbo.DictanceKM(@mylat, Address.latitude, @mylong, Address.longitude)) DistanceKm, * from Address
+SELECT top 5 ABS(dbo.DictanceKM(@mylat, Address.latitude, @mylong, Address.longitude)) DistanceKm, * from HawkerCentres.dbo.Address
 ORDER BY ABS(dbo.DictanceKM(@mylat, Address.latitude, @mylong, Address.longitude)) 
