ae82afd9f6e5fd0c64d74e4e5b2661deb44284b5
Alex <alexbarke002@gmail.com>
2018-04-19 14:52:10 +0800

    added sql query for closest coordinates to given location, added index method to AddressController.cs

--- Controllers/AddressController.cs
+++ Controllers/AddressController.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using HawkerFinder.Data;
 using HawkerFinder.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace HawkerFinder.Controllers
-{
-    public class AddressController : Controller
-    {
-        public IActionResult Index()
-        {
-            
-            return View();
+namespace HawkerFinder.Controllers {
+    public class AddressController : Controller {
+        private readonly HawkerContext _context;
+        public AddressController (HawkerContext context) {
+            _context = context;
+        }
+        public async Task<IActionResult> Index () {
+            return View (await _context.Addresses.ToListAsync ());
         }
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Error () {
+            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
